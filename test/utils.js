689b1620587d5bf91528f3d8024b3a338a087b85
Chris Deacy <chrisdothtml@gmail.com>
2018-12-19 18:27:13 -0800

    updating test format

--- test/utils.js
+++ test/utils.js
@@ -1,8 +1,11 @@
 import { test, assertEqual } from 'https://deno.land/x/testing/testing.ts'
 import { getQuery } from '../lib/utils.js'
 
-test(function util_getQuery () {
-  assertEqual(getQuery('https://hostname?foo=bar&bar=baz'), { foo: 'bar', bar: 'baz' })
-  assertEqual(getQuery('https://hostname?foo&bar=baz'), { foo: true, bar: 'baz' })
-  assertEqual(getQuery('https://hostname?foo=bar%3Fbaz'), { foo: 'bar?baz' })
+test({
+  name: 'getQuery',
+  fn () {
+    assertEqual(getQuery('https://hostname?foo=bar&bar=baz'), { foo: 'bar', bar: 'baz' })
+    assertEqual(getQuery('https://hostname?foo&bar=baz'), { foo: true, bar: 'baz' })
+    assertEqual(getQuery('https://hostname?foo=bar%3Fbaz'), { foo: 'bar?baz' })
+  }
 })
